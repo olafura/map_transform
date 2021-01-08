bcb96980c66263cc092272e660fad99c3480495f
Olafur Arason <olafura@olafura.com>
2021-01-08 01:14:15 +0100

    add license and package info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule MapTransform.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A library to transform between two map structures",
+      package: package()
     ]
   end
 
@@ -21,5 +23,14 @@ defmodule MapTransform.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      maintainers: ["Olafur Arason"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/olafura/map_transform"},
+      files: ~w(lib LICENSE mix.exs README.md)
+    ]
   end
 end
